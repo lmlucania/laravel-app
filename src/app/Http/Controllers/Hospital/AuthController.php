4cdba68d17091e572b8a203c840db7801dc9a472
M.maeda <petit.chat.0509@gmail.com>
2024-10-25 00:10:43 +0900

    docs:apiドキュメント更新

--- src/app/Http/Controllers/Hospital/AuthController.php
+++ src/app/Http/Controllers/Hospital/AuthController.php
@@ -12,6 +12,25 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/hospital/login",
+     *     tags={"Hospital"},
+     *     summary="ログイン",
+     *     @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/Requests~1Hospital~1LoginRequest")
+     *     ),
+     *     @OA\Response(
+     *          response="200",
+     *          description="ログイン成功",
+     *     ),
+     *     @OA\Response(
+     *          response="401",
+     *          description="ログイン失敗",
+     *     )
+     * )
+     */
     public function login(LoginRequest $request)
     {
         if (Auth::guard('staffs')->attempt($request->only(['email', 'password']))) {
@@ -22,6 +41,17 @@ class AuthController extends Controller
         throw new UnauthorizedException('ログインに失敗しました。メールアドレスまたはパスワードが正しくありません。');
     }
 
+    /**
+     * @OA\Post(
+     *     path="/hospital/logout",
+     *     tags={"Hospital"},
+     *     summary="ログアウト",
+     *     @OA\Response(
+     *          response="200",
+     *          description="ログアウト成功",
+     *     ),
+     * )
+     */
     public function logout(Request $request)
     {
         Auth::logout();
